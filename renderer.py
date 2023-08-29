a9586df94fb628c1537f81880e81e2506b2624fa
tez4 <joel.twann@bluewin.ch>
2023-08-29 21:39:15 +0200

    Worked on basic test scene

--- renderer.py
+++ renderer.py
@@ -1,35 +1,74 @@
 import bpy
+import random
 
-bpy.ops.mesh.primitive_cube_add()
-so = bpy.context.active_object
+def create_glowing_object(size = 1, location = (0, 0, 0)):
+    bpy.ops.mesh.primitive_cube_add(size=size, location=location)
+    so = bpy.context.active_object
 
-so.rotation_euler[0] = 5
+    so.rotation_euler[0] = 5
 
-mod_subsurf = so.modifiers.new("My Modifier", "SUBSURF")
-mod_subsurf.levels = 3
+    mod_subsurf = so.modifiers.new("My Modifier", "SUBSURF")
+    mod_subsurf.levels = 3
 
-bpy.ops.object.shade_smooth()
+    bpy.ops.object.shade_smooth()
 
-mod_displace = so.modifiers.new("Displacement", "DISPLACE")
-mod_displace.strength = 0.3
+    mod_displace = so.modifiers.new("Displacement", "DISPLACE")
+    mod_displace.strength = 0.3
 
-new_tex = bpy.data.textures.new("texture", "DISTORTED_NOISE")
-new_tex.noise_scale = 0.5
+    new_tex = bpy.data.textures.new("texture", "DISTORTED_NOISE")
+    new_tex.noise_scale = 0.5
 
-mod_displace.texture = new_tex
+    mod_displace.texture = new_tex
 
-# add material
-new_material = bpy.data.materials.new(name = "My Material")
-so.data.materials.append(new_material)
+    # add material
+    new_material = bpy.data.materials.new(name = "My Material")
+    so.data.materials.append(new_material)
 
-new_material.use_nodes = True
-nodes = new_material.node_tree.nodes
+    new_material.use_nodes = True
+    nodes = new_material.node_tree.nodes
 
-material_output = nodes.get("Material Output")
-node_emission = nodes.new(type='ShaderNodeEmission')
+    material_output = nodes.get("Material Output")
+    node_emission = nodes.new(type='ShaderNodeEmission')
 
-node_emission.inputs[0].default_value = (0.0, 0.3, 1.0, 1)
-node_emission.inputs[1].default_value = 500.0
+    node_emission.inputs[0].default_value = (0.0, 0.3, 1.0, 1)
+    node_emission.inputs[1].default_value = 500.0
 
-links = new_material.node_tree.links
-new_link = links.new(node_emission.outputs[0], material_output.inputs[0])
+    links = new_material.node_tree.links
+    new_link = links.new(node_emission.outputs[0], material_output.inputs[0])
+
+# add base plane
+def create_base_plane():
+    bpy.ops.mesh.primitive_plane_add(size=250, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
+    plane = bpy.context.active_object
+
+    mat = bpy.data.materials.new(name="GlossyWhite")
+
+    mat.use_nodes = True
+    nodes = mat.node_tree.nodes
+
+    # Clear default nodes
+    for node in nodes:
+        nodes.remove(node)
+
+    glossy_shader = nodes.new(type='ShaderNodeBsdfGlossy')
+    glossy_shader.location = (0,0)
+
+    glossy_shader.inputs["Color"].default_value = (1, 1, 1, 1)  # RGB + Alpha
+    glossy_shader.inputs["Roughness"].default_value = 0.1  # Adjust as needed
+
+    # Add a Material Output node and connect the Glossy shader to it
+    material_output = nodes.new(type='ShaderNodeOutputMaterial')
+    material_output.location = (400,0)
+    mat.node_tree.links.new(glossy_shader.outputs["BSDF"], material_output.inputs["Surface"])
+
+    # Assign the material to the plane
+    plane.data.materials.append(mat)
+
+if __name__ == '__main__':
+    for i in range(20):
+        x = random.random() * 100 - 50
+        y = random.random() * 100 - 50
+        
+        create_glowing_object(1, (x, y, 2))
+    
+    create_base_plane()
