c5159bc970e863620c11d62a716bb247c82e165f
tez4 <joel.twann@bluewin.ch>
2023-10-12 19:32:26 +0200

    Updated slurm jobs

--- jobs/singularity_pull.sh
+++ jobs/singularity_pull.sh
@@ -2,10 +2,10 @@
 #SBATCH -p performance
 #SBATCH -t 01:00:00
 #SBATCH --gpus=1
-#SBATCH --job-name=blender_render
+#SBATCH --job-name=singularity_pull
 #SBATCH --out=out.log
 #SBATCH --err=err.log
 
 module load singularity
 
-singularity exec blender.sif blender --background --python functions/renderer.py
+singularity pull shub://blender/blender:3.6
