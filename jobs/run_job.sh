39eb428c59d1fd9e042fd2d79f0b3b2522b27200
tez4 <joel.twann@bluewin.ch>
2023-10-21 11:34:50 +0200

    Made minor changes to job

--- jobs/run_job.sh
+++ jobs/run_job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -p performance
 #SBATCH -t 01:00:00
-#SBATCH --gpus=1
+#SBATCH --gpus=4
 #SBATCH --job-name=blender_render
 #SBATCH --out=out.log
 #SBATCH --err=err.log
 
 module load singularity
 
-singularity exec --nv blender.sif blender --background --python functions/renderer.py
+singularity exec --nvccli blender.sif blender --background --python functions/renderer.py
